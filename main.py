375e5c9803d9e7f192a7c83c3bd5c5cec7b58d02
MariaDeetman <58265395+MariaDeetman@users.noreply.github.com>
2026-02-24 14:10:19 +0100

    Update main.py
    Changed template to code for sending a motivational quote.

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
 import smtplib
-import os
+import datetime as dt
+import random
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+my_email = os.environ.get("MY_EMAIL")
+app_password = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+now = dt.datetime.now()
+weekday = now.weekday()
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+date_of_birth = dt.datetime(year= 1995, month= 11, day= 16)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+if weekday == 4:
+    with open("quotes.txt") as quotes:
+        quotes_list = quotes.readlines()
+        selection = random.choice(quotes_list)
+
+    email_text = (f"Subject: Motivational Quote for Today \n\n"
+                  f"{selection}")
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=my_email, password=app_password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="maria_deetman@hotmail.com",
+                            msg=email_text
+                            )
